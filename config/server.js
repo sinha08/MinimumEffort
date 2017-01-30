3487275e6d819f6c7a63aacf512acaac401bc45d
Apple <apple@Dhaval-Mac.local>
2017-01-30 22:18:33 +0530

    Added first route to dashboard /keb/dashboard

--- config/server.js
+++ config/server.js
@@ -5,9 +5,18 @@ node should be installed on the terminal being used
 */
 var http = require('http');
 
-http.createServer(function( request, response){
-  response.writeHead(200, { 'Content-Type' : 'text/plain'});
-  response.end('Hello World \n');
-}).listen(8082);
+var serverFunction = function( request, response){
+  var req_url = request.url;
+
+    if(req_url === "/keb/dashboard"){
+      response.writeHead(200, { 'Content-Type' : 'text/plain'});
+      response.end('Welcome to KEB Layout \n');
+    }else{
+      response.writeHead(200, { 'Content-Type' : 'text/plain'});
+      response.end('Hello World \n');
+    }
+}
+
+http.createServer(serverFunction).listen(8082);
 
 console.log('Server is listening at http://127.0.0.1:8082/')
